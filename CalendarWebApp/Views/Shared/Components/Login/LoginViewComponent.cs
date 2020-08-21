2467239793577eb87f0d43a2d01fce442dc85bab
WernerWS <WernerWS@DESKTOP-ISHRNCN>
2020-08-21 13:55:22 +0200

    Add project files.

---
+++ CalendarWebApp/Views/Shared/Components/Login/LoginViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarWebApp.Views.Shared.Components
+{
+    //This view component is responsible for controlling login window at home page of application
+    //It is embedded in Home views
+    public class LoginViewComponent : ViewComponent
+    {
+        public LoginViewComponent()
+        {
+        }
+        
+        public IViewComponentResult Invoke()
+        {
+            return View("Login");
+        }
+    }
+}

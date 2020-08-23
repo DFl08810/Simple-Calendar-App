e756329136b2d4684910ee28fc0bcb294e2d9b29
WernerWS <WernerWS@DESKTOP-ISHRNCN>
2020-08-24 00:19:19 +0200

    added data view models

---
+++ CalendarWebApp/Models/EventViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+
+namespace CalendarWebApp.Models
+{
+    public class EventViewModel
+    {
+        public int Id { get; set; }
+        public string SysID { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime EventCreationDateTime { get; set; }
+        public DateTime EventPlannedDateTime { get; set; }
+        public UserViewModel Creator { get; set; }
+        public List<UserViewModel> Attendees { get; set; }
+        public int AttendeesCounter { get; set; }
+
+    }
+}
---
+++ CalendarWebApp/Models/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarWebApp.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string SysID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public DateTime UserCreationDateTime { get; set; }
+        public DateTime UserLastLoginDateTime { get; set; }
+        public int NumCoursesCreated { get; set; }
+    }
+}
